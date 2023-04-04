f07a2c8649ad929a0a802568af5bf3ac5682983e
Anna <zdthns25@gmail.com>
2023-04-04 22:03:02 +0300

    style string-page

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,22 @@
+import style from "./style.module.css";
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={style.body}>
+        <div className={style.wrapper}>
+          <div>
+            <Input />
+            <span className={style.caption}>Максимум — 11 символов</span>
+          </div>
+
+          <Button text="Развернуть" />
+        </div>
+      </div>
     </SolutionLayout>
   );
 };
