c8d8372b14726f5615641f0d2c45901d54c99d1e
Anna <zdthns25@gmail.com>
2023-04-07 20:54:25 +0300

    style queue-page

--- src/components/queue-page/queue-page.tsx
+++ src/components/queue-page/queue-page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import style from "./style.module.css";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const QueuePage: React.FC = () => {
   return (
     <SolutionLayout title="Очередь">
-
+      <div className={style.wrapper}>
+        <form className={style.form}>
+          <Input maxLength={4} />
+          <Button text="Добавить" />
+          <Button text="Удалить" />
+          <Button text="Очистить" />
+        </form>
+        <div className={style.visualBlock}>
+          <Circle />;
+        </div>
+      </div>
     </SolutionLayout>
   );
 };
