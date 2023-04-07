1598372794b07eab0304f593833625772c669575
Anna <zdthns25@gmail.com>
2023-04-07 20:49:07 +0300

    style stack-page

--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Circle } from "../ui/circle/circle";
+import style from "./style.module.css";
+import { Button } from "../ui/button/button";
 
 export const StackPage: React.FC = () => {
   return (
     <SolutionLayout title="Стек">
-
+      <div className={style.wrapper}>
+        <form className={style.form}>
+          <Input
+            maxLength={4}
+            isLimitText={true}
+            placeholder="Введите значение"
+          />
+          <Button text="Добавить" />
+          <Button text="Удалить" />
+          <Button text="Очистить" />
+        </form>
+        <div className={style.visualBlock}>
+          <Circle index={1} />;
+        </div>
+      </div>
     </SolutionLayout>
   );
 };
