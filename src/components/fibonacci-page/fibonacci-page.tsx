05a72d66816d17302826967416c4fec383d1a1c8
Anna <zdthns25@gmail.com>
2023-04-21 17:06:20 +0300

    fibonacci

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -4,50 +4,59 @@ import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
-import SimpleForm from "../ui/form/simple-form";
-import { fib } from "./fibonacciUtils";
+import SimpleForm from "./form/simple-form";
+import { fibIterative } from "./fibonacciUtils";
 
 export const FibonacciPage: React.FC = () => {
-  const [value, setValue] = useState("");
+  const [valueInput, setValueInput] = useState(0);
   const [disabled, setDisabled] = useState<boolean>(true);
   const [loaderBtn, setLoaderBtn] = useState<boolean>(false);
   const [numbers, setNumbers] = useState<number[]>([]);
 
-  const handlerChange = (evt: ChangeEvent<HTMLInputElement>) => {
-    setValue(evt.target.value);
-    if (parseInt(evt.target.value) <= 19 && parseInt(evt.target.value) >= 1) {
+  const handlerChange: React.ChangeEventHandler<HTMLInputElement> = (evt) => {
+    let input = Number(evt.target.value);
+    if (isNaN(input)) {
+      input = 0;
+    }
+    if (input <= 19 && input >= 1) {
       setDisabled(false);
     } else {
       setDisabled(true);
     }
+    setValueInput(input);
   };
-  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    let number = Number(value); // string -> number
-    setLoaderBtn(true);
-    const valueArr: number[] = [];
-    let counter: number = 0;
-    const fibArr = fib(number);
-    setValue(""); // clear input
-    setDisabled(true);
-    const interval = setInterval(() => {
-      valueArr.push(fibArr[counter]);
-      setNumbers([...valueArr]);
 
-      counter++;
-      if (fibArr.length - 1 === valueArr.length - 1) {
-        clearInterval(interval);
-        setLoaderBtn(false);
-      }
-    }, 500);
+  const handlerSubmit: React.FormEventHandler<HTMLButtonElement> = async (
+    evt
+  ) => {
+    evt.preventDefault();
+    setLoaderBtn(true);
+    let input = Number(valueInput);
+    console.log(valueInput);
+    await fibIterative(input + 1, setNumbers);
+    setLoaderBtn(false);
   };
 
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
       <div className={style.wrapper}>
-        <SimpleForm isLimitText={true} maxLength={19} text1={"Рассчитать"} />
+        <SimpleForm
+          placeholder={"Введите число"}
+          isLimitText={true}
+          maxLength={19}
+          text1={"Рассчитать"}
+          linkedList="small"
+          isLoader={loaderBtn}
+          disabled={disabled}
+          handlerSubmit={handlerSubmit}
+          handlerChange={handlerChange}
+          value={valueInput}
+          min={1}
+        />
         <div className={style.animationBlock}>
-          <Circle />
+          {numbers?.map((number, index) => (
+            <Circle letter={`${number}`} key={index} index={index} />
+          ))}
         </div>
       </div>
     </SolutionLayout>
--- src/components/fibonacci-page/fibonacciUtils.ts
+++ src/components/fibonacci-page/fibonacciUtils.ts
@@ -1,26 +1,22 @@
+import { SHORT_DELAY_IN_MS, delay } from "../../constants/delays";
 
 
-//export const fib1 = (n: number): number => {
-//  if (n === 1 || n === 2) {
-//    return 1
-//  }
-//  return fib1(n - 1) + fib1(n - 2)
-//}
+export const fibIterative = async (n: number, setNumber: Function) => {
+  const fibArr = [];
+  for (let i = 0; i < n; i++) {
+    fibArr.push(fib(i));
+    setNumber([...fibArr]);
+    await delay(SHORT_DELAY_IN_MS);
+  }
 
-// или
+}
 
-//const fibIterative = (n: number): number => {
-//  let arr: number[] = [0, 1];
-//  for (let i = 2; i < n + 1; i++) {
-//    arr.push(arr[i - 2] + arr[i - 1])
-//  }
-//  return arr[n]
-//}
-
-// а так еще лучше
-export const fib = (n: number, memo: Record<number, number> = {}): number => {
+const fib = (n: number, memo: Record<number, number> = {}): number => {
   if (n in memo) {
     return memo[n];
+  }
+  if (n === 0) {
+    return 0;
   }
   if (n <= 2) {
     return 1;
