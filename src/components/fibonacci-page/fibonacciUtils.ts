9aade4398dd3a9df3e087f1c5e2749da8b854961
Anna <zdthns25@gmail.com>
2023-04-21 21:50:25 +0300

    fibonacci

--- src/components/fibonacci-page/fibonacciUtils.ts
+++ src/components/fibonacci-page/fibonacciUtils.ts
@@ -15,10 +15,10 @@ const fib = (n: number, memo: Record<number, number> = {}): number => {
   if (n in memo) {
     return memo[n];
   }
-  if (n === 0) {
-    return 0;
+  if (n === 1) {
+    return 1;
   }
-  if (n <= 2) {
+  if (n <= 1) {
     return 1;
   }
   memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
