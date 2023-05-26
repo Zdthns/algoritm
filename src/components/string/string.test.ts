58f54eda43a2488b72011b1d82848f90fc2ef0a4
Анна <89003491+Zdthns@users.noreply.github.com>
2023-05-26 10:10:17 +0300

    Add files via upload

---
+++ src/components/string/string.test.ts
@@ -0,0 +1,30 @@
+import { ItemArray } from "../../../types/element-states";
+import { handlerArray } from "../stringUtils";
+import { stringArr } from "./constansTest";
+
+const fnc = jest.fn();
+describe("Корректно разворачивает строку", () => {
+  it("с чётным количеством символов", async () => {
+    let inArray = stringArr.evenNum.inArray;
+    let outArray = stringArr.evenNum.outArray;
+    expect(await handlerArray(inArray, fnc)).toEqual(outArray);
+  });
+
+  it("с нечетным количеством символов", async () => {
+    let inArray = stringArr.odd.inArray;
+    let outArray = stringArr.odd.outArray;
+    expect(await handlerArray(inArray, fnc)).toEqual(outArray);
+  });
+
+  it("с одним символом.", async () => {
+    let inArray = stringArr.one.inArray;
+    let outArray = stringArr.one.outArray;
+    expect(await handlerArray(inArray, fnc)).toEqual(outArray);
+  });
+
+  it("пустую строку", async () => {
+    let inArray: ItemArray<string>[] = [];
+    let outArray: ItemArray<string>[] = [];
+    expect(await handlerArray(inArray, fnc)).toEqual(outArray);
+  });
+});
